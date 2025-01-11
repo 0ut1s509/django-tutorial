15f782172c444bed0617bb1ca2ed57a1bb1c9436
0ut1s509 <gentlemansondy509@gmail.com>
2025-01-11 16:31:08 -0500

    first commit

---
+++ Desktop/0/django_rest/django_project/blog/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib import messages
+from .models import Post
+from .forms import PostForm
+# Create your views here.
+
+
+
+def home(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request,'home.html',context)
+
+@login_required
+def create_post(request):
+    if request.method =='GET':
+        context = {'form':PostForm()}
+        return render (request,'post_form.html',context)
+    if request.method =='POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The post has been created successfully.')
+            return redirect('posts')
+        else:
+            messages.error(request, 'Please correct the following errors:')
+            return render (request,'post_form.html',context)
+
+@login_required
+def edit_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'GET':
+        context = {'form': PostForm(instance=post), 'id': id}
+        return render(request,'post_form.html',context)
+    
+    elif request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The post has been updated successfully.')
+            return redirect('posts')
+        else:
+            messages.error(request, 'Please correct the following errors:')
+            return render(request,'post_form.html',{'form':form})
+
+@login_required
+def delete_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    context = {'post': post}    
+    
+    if request.method == 'GET':
+        return render(request, 'post_confirm_delete.html',context)
+    elif request.method == 'POST':
+        post.delete()
+        messages.success(request,  'The post has been deleted successfully.')
+        return redirect('posts')
+
+
+def about(request):
+    return render(request,'about.html')
+
